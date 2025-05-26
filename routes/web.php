2e85b5fcfe8f9a9e92d9f03ec8d35c43ffe215d3
MonoZikk <monozikk13@gmail.com>
2025-05-26 19:45:38 +0700

    Menyusun Views

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/loginregister', function () {
+    return view('loginregister.loginregister');
+});
+
+Route::get('/home', function () {
+    return view('home.home');
+});
+
+Route::get('/daftarbuku', function () {
+    return view('daftarbuku.daftarbuku');
+});
